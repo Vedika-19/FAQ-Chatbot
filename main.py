6200d70e9b20eabda86c8829debeca78f60f972a
Rohan Dilip P <73561095+ronie77@users.noreply.github.com>
2021-01-04 12:45:28 +0530

    Create main.py
    Calls the driver class

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from dialogue_sys import *
+ 
+ 
+    
+app = Flask(__name__)
+dialogue_manager = DialogueManager()
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(dialogue_manager.generate_answer(userText))
+ 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
